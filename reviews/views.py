c3d2d82c86724ed5a5930cd73ebb6a2724f1baa5
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-01 21:53:38 -0700

    Add files via upload

--- reviews/views.py
+++ reviews/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to Bookr")
+    name = request.GET.get("name") or " to Bookr"
+    return HttpResponse("Welcome{}!".format(name))
