1dd688a4c213ac238883128ff5afceea9909cd38
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-02 22:46:36 -0700

    Add files via upload

--- reviews/models.py
+++ reviews/models.py
@@ -7,10 +7,11 @@ class Publisher(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=70, help_text="The title of the book.")
-    publication_date = models.DateField(verbose_name = "Date the book was published")
-    isbn = models.CharField(max_length=20, verbose_name= "ISBN number of the book.")
+    publication_date = models.DateField(verbose_name="Date the book was published")
+    isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
 
 class Contributor(models.Model):
-    first_names = models.CharField(max_length=50, help_text = "The contributor's first name or names.")
-    last_names = models.CharField(max_length=50, help_text = "The contributor's last name or names.")
+    first_names = models.CharField(max_length=50, help_text="The contributor's first name or names.")
+    last_names = models.CharField(max_length=50, help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
