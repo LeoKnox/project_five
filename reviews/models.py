2b6ff5d7fcfa2d5435599575dfd996a1213a47ba
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-03 22:35:12 -0700

    Add files via upload

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 class Publisher(models.Model):
     name = models.CharField(max_length=50, help_text="The name of the Publisher")
@@ -16,3 +17,21 @@ class Contributor(models.Model):
     first_names = models.CharField(max_length=50, help_text="The contributor's first name or names.")
     last_names = models.CharField(max_length=50, help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
+
+class BookContributor(models.Model):
+    class ContributionRole(models.TextChoices):
+        AUTHOR = "AUTHOR", "Author"
+        CO_AUTHOR = "CO_AUTHOR", "Co-Author"
+        EDITOR = "EDITOR", "Editor"
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
+    role = models.CharField(verbose_name="The role this contributor had in the book.",
+        choices=ContributionRole.choices, max_length=20)
+
+class Review(models.Model):
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField(help_text="The rating the review has given.")
+    date_created = models.DateTimeField(auto_now_add=True, help_text="The date and time the review was created.")
+    date_edited = models.DateTimeField(null=True, help_text="The date and time the review was last edited.")
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, help_text="The Book that his review is for.")
