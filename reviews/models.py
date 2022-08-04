40297800df860c2e944b19209a0a4a30fe12105e
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-03 22:39:33 -0700

    Add files via upload

--- reviews/models.py
+++ reviews/models.py
@@ -16,10 +16,16 @@ class Book(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     models.ManyToManyField('Contributor', through="BookContributor")
 
+    def __str__(self):
+        return self.title
+
 class Contributor(models.Model):
     first_names = models.CharField(max_length=50, help_text="The contributor's first name or names.")
     last_names = models.CharField(max_length=50, help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
+
+    def __str__(self):
+        return self.first_names
 
 class BookContributor(models.Model):
     class ContributionRole(models.TextChoices):
